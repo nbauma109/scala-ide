0d1d3732e7d1af9ac1951f3fbe04fdd79e18c806
Nicolas Baumann <nicolasbaumann@MacBook-Air-de-Nicolas.local>
2026-03-08 09:35:11 +0100

    Take advantage of set-versions.sh

--- set-versions.sh
+++ set-versions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -euo pipefail
+
+MVN_CMD="${MVN_CMD:-mvn}"
+
 function usage() {
     cat <<EOF
 
@@ -36,32 +40,48 @@ if [ $# -ne 1 ]; then
 fi
 
 function setVersion() {
-    mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1-SNAPSHOT
+    "${MVN_CMD}" -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1-SNAPSHOT
 }
 
-validate $1
+function rootPomVersion() {
+    sed -n 's|^[[:space:]]*<version>\(.*\)</version>[[:space:]]*$|\1|p' pom.xml | head -n 1
+}
+
+function syncNonPomVersions() {
+    local old_snapshot="$1"
+    local new_base="$2"
+    local old_base="${old_snapshot%-SNAPSHOT}"
+    local old_feature="${old_base}.qualifier"
+    local new_snapshot="${new_base}-SNAPSHOT"
+    local new_feature="${new_base}.qualifier"
+
+    while IFS= read -r target_file; do
+        perl -0pi -e "s#(<artifactId>org\\.scala-ide\\.zinc\\.(?:compiler\\.bridge|library)</artifactId>\\s*<version>)\Q${old_snapshot}\E(</version>)#\\1${new_snapshot}\\2#gms" "${target_file}"
+    done < <(git ls-files 'target-platform/*.target')
+
+    while IFS= read -r feature_file; do
+        perl -0pi -e "s#version=\"\Q${old_feature}\E\"#version=\"${new_feature}\"#g" "${feature_file}"
+    done < <(git ls-files '*feature.xml')
+
+    while IFS= read -r manifest_file; do
+        perl -0pi -e "s#(Bundle-Version:\\s*)\Q${old_feature}\E#\\1${new_feature}#g" "${manifest_file}"
+    done < <(git ls-files '*MANIFEST.MF' '*MANIFEST-*.MF')
+}
+
+validate "$1"
 
 echo "Setting version to $1"
 
-setVersion $1
+old_version="$(rootPomVersion)"
+if [[ -z "${old_version}" ]]; then
+    echo "Unable to determine current version from pom.xml"
+    exit 1
+fi
 
-(cd org.scala-ide.build-toolchain && setVersion $1)
-(cd org.scala-ide.sdt.build && setVersion $1)
-(cd org.scala-ide.p2-toolchain && setVersion $1)
+setVersion "$1"
 
-cat <<EOF
-!!!!
-!!!! Some files still need to be updated manually:
-!!!!     <parent> tag of pom.xml in org.scala-ide.{sdt.build,build-toolchain,p2-toolchain}
-!!!!     */resources/{feature,pom}.xml
-!!!!     */resources/META-INF/MANIFEST-*.MF
-!!!!
-!!!! This can be done with the following command:
-!!!!
-!!!!   ag "<old-version>" | awk -F ':' '{print \$1}' | xargs sed -i '' 's/<old-version>/<new-version>/'
-!!!!
-!!!! where <old-version> is the version of the previous release and <new-version> the version of the next release.
-!!!! ag is a tool similar to grep or ack, it is also called "The Silver Searcher": https://github.com/ggreer/the_silver_searcher
-!!!! In constract to grep or ack, ag considers the .gitignore files and therefore makes updating the relevant versions easier.
+(cd org.scala-ide.build-toolchain && setVersion "$1")
+(cd org.scala-ide.sdt.build && setVersion "$1")
+(cd org.scala-ide.p2-toolchain && setVersion "$1")
 
-EOF
+syncNonPomVersions "${old_version}" "$1"
